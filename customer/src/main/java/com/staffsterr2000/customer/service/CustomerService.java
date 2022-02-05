6c4caa6c3956d6a81c63b8c3d43e6c30b3760ac0
staffsterr2000 <rosenkostas@gmail.com>
2022-02-05 21:50:32 +0200

    primary logic implement

---
+++ customer/src/main/java/com/staffsterr2000/customer/service/CustomerService.java
@@ -0,0 +1,21 @@
+package com.staffsterr2000.customer.service;
+
+import com.staffsterr2000.customer.model.entity.Customer;
+import com.staffsterr2000.customer.request.CustomerRegistrationRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public record CustomerService() {
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        log.info("new customer {}", customer);
+    }
+}
