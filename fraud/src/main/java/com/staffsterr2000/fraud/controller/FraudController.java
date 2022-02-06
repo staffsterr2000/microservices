ea525c79a01e1ec63772e89b7827e7759e6257cb
staffsterr2000 <rosenkostas@gmail.com>
2022-02-06 13:02:36 +0200

    logging for fraud module

--- fraud/src/main/java/com/staffsterr2000/fraud/controller/FraudController.java
+++ fraud/src/main/java/com/staffsterr2000/fraud/controller/FraudController.java
@@ -3,11 +3,13 @@ package com.staffsterr2000.fraud.controller;
 import com.staffsterr2000.fraud.response.FraudCheckResponse;
 import com.staffsterr2000.fraud.service.FraudCheckHistoryService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/fraud-check")
 @AllArgsConstructor
@@ -17,6 +19,7 @@ public class FraudController {
 
     @GetMapping("/{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable Long customerId) {
+        log.info("new check request for customer id {}", customerId);
         boolean isFraudulentCustomer = fraudCheckHistoryService.isFraudulentCustomer(customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
