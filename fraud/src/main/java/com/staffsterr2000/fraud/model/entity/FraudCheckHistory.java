a2ab2662f87dd0c7d01d1b44ab16b9ce5e39aa6c
staffsterr2000 <52102812+staffsterr2000@users.noreply.github.com>
2022-02-06 12:41:22 +0200

    Fraud module logic
    * fraud module init
    
    * add starter-web to pom.xml
    
    * base logic for fraud module

---
+++ fraud/src/main/java/com/staffsterr2000/fraud/model/entity/FraudCheckHistory.java
@@ -0,0 +1,33 @@
+package com.staffsterr2000.fraud.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FraudCheckHistory {
+
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence")
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence")
+    private Long id;
+    private Long customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
---
+++ fraud/src/main/java/com/staffsterr2000/fraud/response/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.staffsterr2000.fraud.response;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
